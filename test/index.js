0fe40dc1bfa3a1a74c4d523540295867a265f400
segayuu <segayuu@gmail.com>
2018-04-22 18:36:26 +0900

    [WIP] add mock

--- test/index.js
+++ test/index.js
@@ -1,5 +1,11 @@
 import test from "ava";
+import tester from "../";
 
 test("test", t => {
+    const input = "test";
+    const result = tester.test(input);
+
+    t.deepEqual(input, result);
     t.pass();
 });
+
