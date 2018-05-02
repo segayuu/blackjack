c1449566b7d29a29c81f7c5f8f1428cb73f55927
segayuu <segayuu@gmail.com>
2018-05-02 13:19:58 +0900

    Add Test: Hand#[@@iterator]()

--- test/Hand.js
+++ test/Hand.js
@@ -11,3 +11,20 @@ test("Hand#push()", t => {
 });
 test.todo("Hand#list()");
 test.todo("Hand#clear()");
+test("Hand#[@@iterator]()", t => {
+    const hand = new Hand();
+    const card1 = new Card("spade", 1);
+    const card2 = new Card("spade", 2);
+    const card3 = new Card("spade", 3);
+
+    hand.push(card1);
+    hand.push(card2);
+    hand.push(card3);
+
+    const iter = hand[Symbol.iterator]();
+
+    t.deepEqual({ value: card1, done: false }, iter.next());
+    t.deepEqual({ value: card2, done: false }, iter.next());
+    t.deepEqual({ value: card3, done: false }, iter.next());
+    t.true(iter.next().done);
+});
