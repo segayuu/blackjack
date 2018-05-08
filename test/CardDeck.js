abd824855d65d88a89fbdc23bf9fce04426f33f5
segayuu <segayuu@gmail.com>
2018-05-08 12:05:12 +0900

    test sort

--- test/CardDeck.js
+++ test/CardDeck.js
@@ -1,6 +1,78 @@
 const test = require("ava");
 const { CardDeck } = require("../lib/CardDeck");
 
+test("CardDeck#pull()", t => {
+    const deck = new CardDeck();
+    const { toString } = Object.prototype;
+    let card;
+
+    while (typeof (card = deck.pull()) === "object") {
+        t.deepEqual(toString.call(card), "[object Card]");
+    }
+    t.deepEqual(deck.pull(), void 0);
+    t.deepEqual(deck.pull(), void 0);
+});
+
+test("CardDeck#count", t => {
+    const deck = new CardDeck();
+    let sum = deck.count;
+
+    while (deck.count) {
+        deck.pull();
+        t.deepEqual(deck.count, --sum);
+    }
+    t.deepEqual(deck.count, 0);
+});
+
+test("CardDeck#next()", t => {
+    const deck = new CardDeck();
+    const { toString } = Object.prototype;
+
+    while (deck.count) {
+        const { done, value } = deck.next();
+
+        t.false(done);
+        t.deepEqual(toString.call(value), "[object Card]");
+    }
+
+    const { done, value } = deck.next();
+
+    t.true(done);
+    t.deepEqual(value, void 0);
+    t.deepEqual(deck.count, 0);
+    t.deepEqual(deck.pull(), void 0);
+});
+
+test("CardDeck#[@@iterator]()", t => {
+    const deck = new CardDeck();
+    const { toString } = Object.prototype;
+
+    for (const card of deck) {
+        t.deepEqual(toString.call(card), "[object Card]");
+    }
+    t.deepEqual(deck.count, 0);
+    t.deepEqual(deck.pull(), void 0);
+});
+
+test("CardDeck#toArray()", t => {
+    const deck = new CardDeck();
+    const { count } = deck;
+    const result = deck.toArray();
+
+    t.deepEqual(deck.count, count, "CardDeck.count property should not be changed when calling CardDeck#toArray()!");
+    t.deepEqual(result, Array.from(deck));
+});
+
+test("CardDeck#values()", t => {
+    const deck = new CardDeck();
+    const { count } = deck;
+    const result = deck.values();
+
+    t.deepEqual(deck.count, count, "CardDeck.count property should not be changed when calling CardDeck#values()!");
+    t.deepEqual(typeof result.next, "function");
+    t.deepEqual(Array.from(result), deck.toArray());
+});
+
 test("CardDeck#reset() - all includes", t => {
     const deck = new CardDeck();
     const suits = ["spade", "slub", "diamond", "heart"];
@@ -26,80 +98,4 @@ test("CardDeck#reset() - isShuffle", t => {
 
     t.true(deck.count !== 0);
     t.notDeepEqual(copy, results, "The order of the cards has not been changed.");
-});
-
-test("CardDeck#count", t => {
-    const deck = new CardDeck();
-    let sum = deck.count;
-
-    while (deck.count) {
-        deck.pull();
-        t.deepEqual(deck.count, deck.toArray().length);
-        t.deepEqual(deck.count, --sum);
-    }
-    t.deepEqual(deck.count, 0);
-});
-
-test("CardDeck#pull()", t => {
-    const deck = new CardDeck();
-    const { toString } = Object.prototype;
-    let card;
-
-    while (typeof (card = deck.pull()) === "object") {
-        t.deepEqual(toString.call(card), "[object Card]");
-    }
-    t.deepEqual(deck.count, 0);
-    t.deepEqual(deck.pull(), void 0);
-});
-
-test("CardDeck#[@@iterator]()", t => {
-    const deck = new CardDeck();
-    const { toString } = Object.prototype;
-
-    for (const card of deck) {
-        t.deepEqual(toString.call(card), "[object Card]");
-    }
-    t.deepEqual(deck.count, 0);
-    t.deepEqual(deck.pull(), void 0);
-});
-
-test("CardDeck#next()", t => {
-    const deck = new CardDeck();
-    const { toString } = Object.prototype;
-
-    while (deck.count) {
-        const { done, value } = deck.next();
-
-        t.false(done);
-        t.deepEqual(toString.call(value), "[object Card]");
-    }
-
-    const { done, value } = deck.next();
-
-    t.true(done);
-    t.deepEqual(value, void 0);
-    t.deepEqual(deck.count, 0);
-    t.deepEqual(deck.pull(), void 0);
-});
-
-test("CardDeck#toArray()", t => {
-    const deck = new CardDeck();
-    const { count } = deck;
-    const array = deck.toArray();
-
-    t.deepEqual(deck.count, count);
-
-    const result = Array.from(deck);
-
-    t.deepEqual(result, array);
-});
-
-test("CardDeck#values()", t => {
-    const deck = new CardDeck();
-    const { count } = deck;
-    const array = deck.toArray();
-    const result = Array.from(deck.values());
-
-    t.deepEqual(result, array);
-    t.deepEqual(deck.count, count);
 });
